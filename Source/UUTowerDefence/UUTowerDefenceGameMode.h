6bf4daf96fdb07b2536b622e47ffdce9751002cf
TheDeathsinger <tarnthelos@gmail.com>
2015-10-01 23:43:39 -0400

    Added base required files for Unreal project

---
+++ Source/UUTowerDefence/UUTowerDefenceGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UUTowerDefenceGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UUTOWERDEFENCE_API AUUTowerDefenceGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
